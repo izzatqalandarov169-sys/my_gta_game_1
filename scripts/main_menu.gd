65c97e3d3a823e561705cf91fa3e3c99a79a96e8
izzatqalandarov169-sys <izzatqalandarov169@gmail.com>
2026-08-20 18:32:28 +0500

    Wire redesigned main menu buttons

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,21 +1,39 @@
 extends Control
 class_name MainMenu
 
-signal start_game_pressed
+signal open_world_pressed
+signal multiplayer_pressed
+signal garage_pressed
+signal weapon_shop_pressed
 signal settings_pressed
 signal quit_pressed
 
-@onready var start_button: Button = $Center/VBox/StartButton
-@onready var settings_button: Button = $Center/VBox/SettingsButton
-@onready var quit_button: Button = $Center/VBox/QuitButton
+@onready var open_world_button: Button = $MenuGrid/OpenWorldButton
+@onready var multiplayer_button: Button = $MenuGrid/MultiplayerButton
+@onready var garage_button: Button = $MenuGrid/GarageButton
+@onready var weapon_shop_button: Button = $MenuGrid/WeaponShopButton
+@onready var settings_button: Button = $Bottom/SettingsButton
+@onready var quit_button: Button = $Bottom/QuitButton
 
 func _ready() -> void:
-    start_button.pressed.connect(_on_start)
+    open_world_button.pressed.connect(_on_open_world)
+    multiplayer_button.pressed.connect(_on_multiplayer)
+    garage_button.pressed.connect(_on_garage)
+    weapon_shop_button.pressed.connect(_on_weapon_shop)
     settings_button.pressed.connect(_on_settings)
     quit_button.pressed.connect(_on_quit)
 
-func _on_start() -> void:
-    start_game_pressed.emit()
+func _on_open_world() -> void:
+    open_world_pressed.emit()
+
+func _on_multiplayer() -> void:
+    multiplayer_pressed.emit()
+
+func _on_garage() -> void:
+    garage_pressed.emit()
+
+func _on_weapon_shop() -> void:
+    weapon_shop_pressed.emit()
 
 func _on_settings() -> void:
     settings_pressed.emit()
