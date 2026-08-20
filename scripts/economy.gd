3410aef347dc6137552edb2aa7dcdb9f9a9ad6d9
izzatqalandarov169-sys <izzatqalandarov169@gmail.com>
2026-08-21 01:31:44 +0500

    Convert game economy to Uzbek so'm UZS

--- scripts/economy.gd
+++ scripts/economy.gd
@@ -1,36 +1,47 @@
 extends Node
 class_name EconomySystem
 
-var balance: int = 1000
+const CURRENCY := "UZS"
+const CURRENCY_NAME := "so‘m"
+
+var balance: int = 100000
 var owned_vehicles: Array[String] = []
 var owned_businesses: Array[String] = []
 
 var jobs := {
-    "taxi": 150,
-    "delivery": 220,
-    "bus_driver": 260,
-    "mechanic": 320,
-    "cargo": 500,
-    "racing": 750,
-    "farm": 180,
-    "security": 300
+    "taxi": 150000,
+    "delivery": 220000,
+    "bus_driver": 260000,
+    "mechanic": 320000,
+    "cargo": 500000,
+    "racing": 750000,
+    "farm": 180000,
+    "security": 300000
 }
 
-func can_afford(price: int) -> bool:
-    return balance >= price
+func format_uzs(amount: int) -> String:
+    var text := str(amount)
+    var result := ""
+    while text.length() > 3:
+        result = " " + text.substr(text.length() - 3, 3) + result
+        text = text.substr(0, text.length() - 3)
+    return text + result + " so‘m"
 
-func earn(amount: int) -> void:
-    if amount > 0:
-        balance += amount
+func can_afford(price_uzs: int) -> bool:
+    return price_uzs >= 0 and balance >= price_uzs
 
-func spend(amount: int) -> bool:
-    if amount < 0 or balance < amount:
+func earn(amount_uzs: int) -> void:
+    if amount_uzs > 0:
+        balance += amount_uzs
+
+func spend(amount_uzs: int) -> bool:
+    if amount_uzs < 0 or balance < amount_uzs:
         return false
-    balance -= amount
+    balance -= amount_uzs
     return true
 
-func buy_vehicle(vehicle_name: String, price: int) -> bool:
-    if not spend(price):
+func buy_vehicle(vehicle_name: String, price_uzs: int) -> bool:
+    if owned_vehicles.has(vehicle_name) or not spend(price_uzs):
         return false
     owned_vehicles.append(vehicle_name)
     return true
@@ -38,6 +49,6 @@ func buy_vehicle(vehicle_name: String, price: int) -> bool:
 func do_job(job_name: String) -> int:
     if not jobs.has(job_name):
         return 0
-    var reward: int = jobs[job_name]
-    earn(reward)
-    return reward
+    var reward_uzs: int = jobs[job_name]
+    earn(reward_uzs)
+    return reward_uzs
